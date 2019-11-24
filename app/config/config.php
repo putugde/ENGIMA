5de14c40b413cf4605c4e5ab454de6ccee895243
Putu Gde Aditya Taguh Widiana <putu.widiana@ibm-jti.com>
2019-11-24 16:33:24 +0700

    Revert "Merge branch 'transaction-history' into 'master'"
    This reverts commit 6f18d827455d6767a782936bbb3bafd805106be4, reversing
    changes made to fe7454a94267174fd14164a43e063fe1763685e3.

--- app/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('BASEURL', 'http://localhost/engima/public');
-define('DB_HOST', '54.226.75.201');
-define('DB_UNAME', 'root');
-define('DB_PSW', '');
-define('DB_NAME', 'engima');
--- app/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'core/app.php';
-require_once 'core/controller.php';
-require_once 'core/database.php';
-require_once 'config/config.php';
